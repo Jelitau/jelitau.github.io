302138995b2e025e61ae38f72fdaebd9be5bfa6f
The_Elite_Knight <57091080+Elite0Knight@users.noreply.github.com>
2022-05-08 22:02:42 +0200

    added button

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,20 @@
-const myHeading = document.querySelector('h1');
-document.querySelector('html').addEventListener('click', function() {
-    alert('Iiieeek, stop touching me!');
-  });
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hiiii to my Play Ground, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+function setUserName() {
+  let myName = prompt('Please enter your name. \nYour Name will be stored locally on your Device.', 'Username');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Hiiii to my Play Ground, ' + myName;
+}
